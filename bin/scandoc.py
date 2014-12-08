ffe527c6f2eb1d73d5a65fef25acfe27c320d7d1
Eliot <saltycrane@gmail.com>
2014-12-08 09:06:36 -0800

    add options to scandoc.py

--- bin/scandoc.py
+++ bin/scandoc.py
@@ -2,17 +2,46 @@
 """
 usage: insert paper into scanner, then run ./scandoc.py
 """
+import optparse
 import os
 
-cmd = ' '.join([
-#        'sudo',
-        'scanimage',
-#        '--device-name=fujitsu:libusb:007:030',
-        '--source "ADF Duplex"',
-        '--mode Color',
-        '-v',
-        '--resolution 200',
-        '--y-resolution 200',
-        '--batch',
-        ])
-os.system(cmd)
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option(
+        "-p", "--topdf",
+        action="store_true",
+        dest="topdf",
+        default=False,
+        help="convert to PDF (default False)",
+    )
+    parser.add_option(
+        "-o", "--output",
+        action="store",  # optional because action defaults to "store"
+        dest="outputfile",
+        default="out.pnm",
+        help="output file name",)
+    (options, args) = parser.parse_args()
+
+    cmd = ' '.join([
+    #        'sudo',
+            'scanimage',
+    #        '--device-name=fujitsu:libusb:007:030',
+            # '--source "ADF Duplex"',
+            '--mode Color',
+            '-v',
+            '--resolution 200',
+            # '--y-resolution 200',
+            '--batch',
+            ])
+    os.system(cmd)
+
+    if options.topdf:
+        os.system('~/bin/convert_pnm_to_pdf.py out1.pnm')
+
+        if options.outputfile:
+            os.system('mv out1.pdf %s' % options.outputfile)
+
+
+if __name__ == '__main__':
+    main()
